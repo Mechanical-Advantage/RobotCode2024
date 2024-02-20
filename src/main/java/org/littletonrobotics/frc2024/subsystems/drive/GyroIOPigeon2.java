4de49c353bf3a70e65a910bf17ad097999779ffb
suryatho <suryathoppae@gmail.com>
2024-02-19 22:57:23 -0500

    Use isOK() for checking if gyro connected

--- src/main/java/org/littletonrobotics/frc2024/subsystems/drive/GyroIOPigeon2.java
+++ src/main/java/org/littletonrobotics/frc2024/subsystems/drive/GyroIOPigeon2.java
@@ -47,7 +47,7 @@ public class GyroIOPigeon2 implements GyroIO {
 
   @Override
   public void updateInputs(GyroIOInputs inputs) {
-    inputs.connected = BaseStatusSignal.refreshAll(yaw, yawVelocity).equals(StatusCode.OK);
+    inputs.connected = BaseStatusSignal.refreshAll(yaw, yawVelocity).isOK();
     inputs.yawPosition = Rotation2d.fromDegrees(yaw.getValueAsDouble());
     inputs.yawVelocityRadPerSec = Units.degreesToRadians(yawVelocity.getValueAsDouble());
 

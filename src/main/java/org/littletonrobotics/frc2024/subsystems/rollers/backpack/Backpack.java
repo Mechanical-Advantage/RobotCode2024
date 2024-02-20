76ef764b4463d3f6084328dfb205c6536cb5f011
Jonah <47046556+jwbonner@users.noreply.github.com>
2024-02-19 19:35:45 -0500

    Add backpack and amp controls

---
+++ src/main/java/org/littletonrobotics/frc2024/subsystems/rollers/backpack/Backpack.java
@@ -0,0 +1,35 @@
+// Copyright (c) 2024 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package org.littletonrobotics.frc2024.subsystems.rollers.backpack;
+
+import java.util.function.DoubleSupplier;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.littletonrobotics.frc2024.subsystems.rollers.GenericRollerSystem;
+import org.littletonrobotics.frc2024.subsystems.rollers.GenericRollerSystem.VoltageGoal;
+import org.littletonrobotics.frc2024.util.LoggedTunableNumber;
+
+@Setter
+@Getter
+public class Backpack extends GenericRollerSystem<Backpack.Goal> {
+  @RequiredArgsConstructor
+  @Getter
+  public enum Goal implements VoltageGoal {
+    IDLING(() -> 0),
+    AMP_SCORING(new LoggedTunableNumber("Backpack/AmpScoringVoltage", 12.0));
+
+    private final DoubleSupplier voltageSupplier;
+  }
+
+  private Goal goal = Goal.IDLING;
+
+  public Backpack(BackpackIO io) {
+    super("Backpack", io);
+  }
+}

76d571882c28b9a3574f4faedc11039991d056ef
Jonah <47046556+jwbonner@users.noreply.github.com>
2024-02-19 21:58:26 -0500

    Fix formatting

--- src/main/java/org/littletonrobotics/frc2024/Robot.java
+++ src/main/java/org/littletonrobotics/frc2024/Robot.java
@@ -152,7 +152,7 @@ public class Robot extends LoggedRobot {
 
     // Robot container periodic methods
     robotContainer.checkControllers();
-    
+
     // Log CANivore status
     if (Constants.getMode() == Mode.REAL) {
       var canivoreStatus = CANBus.getStatus("canivore");

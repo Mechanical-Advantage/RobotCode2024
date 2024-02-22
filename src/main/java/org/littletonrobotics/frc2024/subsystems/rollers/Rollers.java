cfcb314e4b1767e0e89b921afab5235be59e3ff5
nharnwal <neelaksh.harnwal@gmail.com>
2024-02-21 21:25:20 -0500

    Add note visualizer for roller quick feed

--- src/main/java/org/littletonrobotics/frc2024/subsystems/rollers/Rollers.java
+++ src/main/java/org/littletonrobotics/frc2024/subsystems/rollers/Rollers.java
@@ -160,6 +160,7 @@ public class Rollers extends SubsystemBase {
 
   public Command quickFeed() {
     return startEnd(() -> goal = Goal.QUICK_INTAKE_TO_FEED, this::goIdle)
+            .alongWith(NoteVisualizer.shoot())
         .withName("Rollers Quick Feed");
   }
 

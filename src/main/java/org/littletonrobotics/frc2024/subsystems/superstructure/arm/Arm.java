c2fa7bbc5b229998a273706f24797cde757257b1
suryatho <suryathoppae@gmail.com>
2024-02-23 04:02:37 -0500

    Fix arm disable and coast logic

--- src/main/java/org/littletonrobotics/frc2024/subsystems/superstructure/arm/Arm.java
+++ src/main/java/org/littletonrobotics/frc2024/subsystems/superstructure/arm/Arm.java
@@ -88,12 +88,13 @@ public class Arm {
   private final Alert absoluteEncoderDisconnected =
       new Alert("Arm absolute encoder disconnected!", Alert.AlertType.WARNING);
 
-  private BooleanSupplier disableOverride = () -> false;
-  private BooleanSupplier coastOverride = () -> false;
+  private BooleanSupplier disableSupplier = DriverStation::isDisabled;
+  private BooleanSupplier coastSupplier = () -> false;
+  private boolean brakeModeEnabled = true;
 
   public Arm(ArmIO io) {
     this.io = io;
-    io.setBrakeMode(true);
+    setBrakeMode(true);
 
     motionProfile =
         new TrapezoidProfile(
@@ -109,8 +110,8 @@ public class Arm {
   }
 
   public void setOverrides(BooleanSupplier disableOverride, BooleanSupplier coastOverride) {
-    this.disableOverride = disableOverride;
-    this.coastOverride = coastOverride;
+    disableSupplier = () -> disableOverride.getAsBoolean() || DriverStation.isDisabled();
+    coastSupplier = coastOverride;
   }
 
   public void periodic() {
@@ -123,9 +124,6 @@ public class Arm {
     followerMotorDisconnected.set(!inputs.followerMotorConnected);
     absoluteEncoderDisconnected.set(!inputs.absoluteEncoderConnected);
 
-    // Set coast mode
-    io.setBrakeMode(coastOverride.getAsBoolean());
-
     // Update controllers
     LoggedTunableNumber.ifChanged(
         hashCode(), () -> io.setPID(kP.get(), kI.get(), kD.get()), kP, kI, kD);
@@ -136,7 +134,6 @@ public class Arm {
         kG,
         kV,
         kA);
-
     LoggedTunableNumber.ifChanged(
         hashCode(),
         constraints ->
@@ -146,8 +143,18 @@ public class Arm {
         maxVelocity,
         maxAcceleration);
 
-    // Don't run profile when characterizing or if coast mode
-    if (!characterizing || !coastOverride.getAsBoolean()) {
+    // Check if disabled
+    if (disableSupplier.getAsBoolean()) {
+      io.stop();
+      // Reset profile when disabled
+      setpointState = new TrapezoidProfile.State(inputs.armPositionRads, 0);
+    }
+
+    // Set coast mode with override
+    setBrakeMode(!coastSupplier.getAsBoolean() || DriverStation.isEnabled());
+
+    // Don't run profile when characterizing, coast mode, or disabled
+    if (!characterizing && brakeModeEnabled && !disableSupplier.getAsBoolean()) {
       // Run closed loop
       setpointState =
           motionProfile.calculate(
@@ -162,13 +169,6 @@ public class Arm {
 
       io.runSetpoint(
           setpointState.position, ff.calculate(setpointState.position, setpointState.velocity));
-    }
-
-    // Check if disabled
-    if (DriverStation.isDisabled() || disableOverride.getAsBoolean()) {
-      io.stop();
-      // Reset profile when disabled
-      setpointState = new TrapezoidProfile.State(inputs.armPositionRads, 0);
     }
 
     // Logs
@@ -188,6 +188,12 @@ public class Arm {
   @AutoLogOutput(key = "Arm/AtGoal")
   public boolean atGoal() {
     return EqualsUtil.epsilonEquals(setpointState.position, goal.getRads(), 1e-3);
+  }
+
+  public void setBrakeMode(boolean enabled) {
+    if (brakeModeEnabled == enabled) return;
+    brakeModeEnabled = enabled;
+    io.setBrakeMode(brakeModeEnabled);
   }
 
   public Command getStaticCurrent() {

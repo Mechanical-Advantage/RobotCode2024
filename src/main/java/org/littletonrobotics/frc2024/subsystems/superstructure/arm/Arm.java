0d1418531c9184bb491f59d8e07c3a2b6a5f2bb9
suryatho <suryathoppae@gmail.com>
2024-02-23 20:05:32 -0500

    fix merge

--- src/main/java/org/littletonrobotics/frc2024/subsystems/superstructure/arm/Arm.java
+++ src/main/java/org/littletonrobotics/frc2024/subsystems/superstructure/arm/Arm.java
@@ -16,8 +16,6 @@ import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.util.Color;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 import lombok.Getter;

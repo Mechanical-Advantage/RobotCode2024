e517f75a66a9cfda8907b84481c158bab28f64b8
suryatho <suryathoppae@gmail.com>
2024-02-29 17:42:24 -0500

    Change led length to 12

--- src/main/java/org/littletonrobotics/frc2024/subsystems/leds/Leds.java
+++ src/main/java/org/littletonrobotics/frc2024/subsystems/leds/Leds.java
@@ -61,7 +61,7 @@ public class Leds extends VirtualSubsystem {
   // Constants
   private static final boolean prideLeds = false;
   private static final int minLoopCycleCount = 10;
-  private static final int length = 43;
+  private static final int length = 12;
   private static final int staticSectionLength = 3;
   private static final double strobeFastDuration = 0.1;
   private static final double strobeSlowDuration = 0.2;

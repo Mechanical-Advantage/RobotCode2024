b43f25dcd2b25708fa09660a2fbf6cda4bf7dac4
Cameron Earle <camearle20@me.com>
2024-03-05 20:21:52 -0500

    Remove climber homing logic

--- src/main/java/org/littletonrobotics/frc2024/subsystems/superstructure/climber/Climber.java
+++ src/main/java/org/littletonrobotics/frc2024/subsystems/superstructure/climber/Climber.java
@@ -7,15 +7,12 @@
 
 package org.littletonrobotics.frc2024.subsystems.superstructure.climber;
 
-import edu.wpi.first.wpilibj.DriverStation;
 import java.util.function.DoubleSupplier;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.littletonrobotics.frc2024.subsystems.superstructure.GenericSlamElevator;
-import org.littletonrobotics.frc2024.subsystems.superstructure.GenericSlamElevator.SlamElevatorGoal;
 import org.littletonrobotics.frc2024.util.LoggedTunableNumber;
-import org.littletonrobotics.junction.Logger;
 
 @Getter
 @Setter
@@ -46,49 +43,8 @@ public class Climber extends GenericSlamElevator<Climber.Goal> {
   }
 
   private Goal goal = Goal.IDLE;
-  private boolean hasHome = false;
-  private double topHome = 0.0;
-
-  private boolean hasCheckedCancel = false;
-  @Getter private boolean requestCancelClimb = false;
 
   public Climber(ClimberIO io) {
     super("Climber", io, 0.4, 0.1);
-  }
-
-  @Override
-  public void periodic() {
-    super.periodic();
-
-    if (getGoal().getState() != SlamElevatorState.RETRACTING) {
-      hasCheckedCancel = false;
-      requestCancelClimb = false;
-    }
-
-    if (extended()) {
-      hasHome = true;
-      topHome = inputs.positionRads;
-    }
-
-    if (DriverStation.isDisabled()) {
-      hasHome = false;
-      hasCheckedCancel = false;
-    }
-
-    // Stop climbing if we are applying too much current
-    // Superstructure cancel climb
-    if (hasHome
-        && Math.abs((inputs.positionRads - topHome) * drumRadiusInches)
-            >= cancelClimbCheckInches.get()
-        && !hasCheckedCancel) {
-      if (Math.abs(inputs.torqueCurrentAmps) <= cancelClimbMaxTorqueCurrent.get()) {
-        // requestCancelClimb = true;
-      }
-      hasCheckedCancel = true;
-    }
-
-    Logger.recordOutput("Superstructure/Climber/HasHome", hasHome);
-    Logger.recordOutput("Superstructure/Climber/HasCheckedCancel", hasCheckedCancel);
-    Logger.recordOutput("Superstructure/Climber/RequestedCancelClimb", requestCancelClimb);
   }
 }

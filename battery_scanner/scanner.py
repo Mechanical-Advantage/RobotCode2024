115daf5291cda8eb1a2ac769e4d1294e0d1d1c55
Leo Huang <panda1931lh@gmail.com>
2024-03-09 14:53:56 -0500

    Initial implementation of network tables

--- battery_scanner/scanner.py
+++ battery_scanner/scanner.py
@@ -1,31 +1,52 @@
+import argparse
+from time import sleep
 import serial
 import array
 import ntcore
+import logging
+import re
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--ip", default="192.168.0.1", type=str, help="IP address to connect to")
+parser.add_argument("--dev", default="COM8", type=str, help="Path to the camera device")
+args = parser.parse_args()
 
 timeout = 5
 prefix = array.array('B', [0x02, 0x00, 0x00, 0x01, 0x00, 0x33, 0x31])
 scan_command = array.array('B',[0x7e, 0x00, 0x08, 0x01, 0x00, 0x02, 0x01, 0xab, 0xcd])
 name_length = 8
 response_length = len(prefix) + name_length
-nt_table = ntcore.NetworkTableInstance.getDefault().getTable(
-                "/" + config_store.local_config.device_id + "/output")
+name = None
+client = ntcore.NetworkTableInstance.getDefault()
+client.startClient4(f'battery-scanner')
+client.setServerTeam(6328)
 
-with serial.Serial('COM3', 9600, timeout=timeout) as ser:
+pub = client.getStringTopic("/battery_name").publish()
+
+with serial.Serial(args.dev, 9600, timeout=timeout) as ser:
+  logging.info("Scanning for battery ID")
+  while True:
+    ser.write(scan_command)
+    response = ser.read(response_length)
     
-    while True:
-        ser.write(scan_command)
-        response = ser.read(response_length)
-        
-        if len(response) == response_length:
-            if response.startswith(prefix):
-                print(f'prefix matches {response.hex()}')
-                print(f"name: {response[-8:]}")
-                nt_table.append(f"Battery name: {response[-8]}")
-                
+    if len(response) == response_length:
+        if response.startswith(prefix):
+            name_bytes = response[-8:]
+            name = name_bytes.decode("utf-8")
+            if re.match('^[a-zA-Z0-9_\\-]+$', name):
+                logging.info(f"Battery ID: {name}")
+                break
             else:
-                print(f'prefix doesnt match {response.hex()}')
-            print('too short')
-            
-       
-    
+                logging.warning("Battery ID doesn't match expected pattern")
 
+if name is not None:
+  pub.set(name)
+else:
+  logging.error("Unable to read battery code")
+
+logging.info("Switching to idle mode")
+while True:
+  sleep(100)

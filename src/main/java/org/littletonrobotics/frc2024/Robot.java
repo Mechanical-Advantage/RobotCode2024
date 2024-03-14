92fac08340bfdad7fe0e0ec362d8c2263ac16d3d
Jonah <47046556+jwbonner@users.noreply.github.com>
2024-03-14 17:01:58 -0400

    Fix formatting

--- src/main/java/org/littletonrobotics/frc2024/Robot.java
+++ src/main/java/org/littletonrobotics/frc2024/Robot.java
@@ -14,7 +14,6 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.Threads;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import java.io.File;

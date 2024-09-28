5d336b6867a2ff46bcb5941e502051cbede7adb2
Cameron Earle <cameronearle@Camerons-MacBook-Pro-2.local>
2024-09-27 17:09:19 -0700

    Update at 'Fri Sep 27 17:09:19 PDT 2024'

--- src/main/java/org/littletonrobotics/frc2024/subsystems/drive/DriveConstants.java
+++ src/main/java/org/littletonrobotics/frc2024/subsystems/drive/DriveConstants.java
@@ -26,7 +26,7 @@ public final class DriveConstants {
       switch (Constants.getRobot()) {
         case SIMBOT, COMPBOT ->
             DriveConfig.builder()
-                .wheelRadius(Units.inchesToMeters(1.891))
+                .wheelRadius(Units.inchesToMeters(2.0109244068301235))
                 .trackWidthX(Units.inchesToMeters(20.75))
                 .trackWidthY(Units.inchesToMeters(20.75))
                 .bumperWidthX(Units.inchesToMeters(37))
